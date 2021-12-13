c8ab136384001afc9860866e3c34f9e167cd3da2
anhnguyet2305 <sonbnnth2007009@fpt.edu.vn>
2021-12-13 09:44:21 +0700

    Add project files.

---
+++ T2008M_NetCoreApi/Controllers/StudentsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using T2008M_NetCoreApi.Models;
+using T2008M_NetCoreApi.Repositories;
+
+namespace T2008M_NetCoreApi.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]    
+    public class StudentsController : ControllerBase
+    {
+        private IStudentsRepository repository;
+
+        public StudentsController(IStudentsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET: api/Categories
+        [HttpGet]
+        public async Task<IEnumerable<Student>> GetCategories()
+        {
+            return await repository.GetStudentsAsync();
+        }
+
+        
+    }
+}
---
+++ T2008M_NetCoreApi/Repositories/StudentsRepository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using T2008M_NetCoreApi.Models;
+
+namespace T2008M_NetCoreApi.Repositories
+{
+    public class StudentsRepository : IStudentsRepository, IDisposable
+    {
+        private DatabaseContext context;      
+
+        public StudentsRepository(DatabaseContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IEnumerable<Student>> GetStudentsAsync()
+        {
+            return await context.Students.ToListAsync();
+        }
+        public async Task<Student> GetStudentByIdAsync(int id)
+        {
+            return await context.Students.FindAsync(id);
+        }
+
+        public async Task AddStudentAsync(Student student)
+        {
+            await context.Students.AddAsync(student);            
+        }
+
+        public async Task DeleteStudentAsync(int id)
+        {
+            Student ctg = await context.Students.FindAsync(id);
+            context.Students.Remove(ctg);
+        }
+
+        public async Task UpdateStudentAsync(Student student)
+        {
+            var studentInDb = await context.Students.FindAsync(student.Id);
+            studentInDb = student;
+            await SaveChangesAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
